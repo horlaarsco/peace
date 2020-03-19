c9e0fbc504d436624dad396e6cd354d5705f3a6f
Darey <horla@Dareys-MacBook-Pro.local>
2020-03-19 21:23:27 +0100

    olaolddu

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,21 +5,21 @@ export class Skills extends Component {
     return (
       <div className='half2 pos-rel ' id='cardss'>
         <div className='pos-abs yellow'>
-          <img className='pos-rel ' src='./Bitmap-1.svg' alt='' />
+          <img className='pos-rel ' src='./Bitmap-1.png' alt='' />
           <p className='texts pos-abs ux'>UX Research</p>
         </div>
         <div className='pos-abs peach'>
-          <img className='pos-rel' src='./Bitmap-2.svg' alt='' />
+          <img className='pos-rel' src='./Bitmap-2.png' alt='' />
           <p className='texts pos-abs'>Wireframing and Prototyping</p>
         </div>
         <div className='pos-abs blue'>
-          <img className='pos-rel' src='./Bitmap-3.svg' alt='' />
+          <img className='pos-rel' src='./Bitmap-3.png' alt='' />
           <p className='texts pos-abs sorting'>
             Personas Mapping and card Sorting
           </p>
         </div>
         <div className='pos-abs pink'>
-          <img className='pos-rel' src='./Bitmap-4.svg' alt='' />
+          <img className='pos-rel' src='./Bitmap-4.png' alt='' />
           <p className='texts pos-abs'>Idea Validation</p>
         </div>
       </div>
