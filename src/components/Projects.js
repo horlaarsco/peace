233f68117a36b2c3ec30f3122ab0f51e68c68186
Darey <horla@Dareys-MacBook-Pro.local>
2020-04-01 01:53:20 +0100

    lola

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -45,27 +45,32 @@ const HALF2 = styled.div`
   align-items: center;
 `;
 
-
-let grey = {}
-
- 
-
+let grey = {};
 
 function Projects() {
   const [images, setlistimages] = useState([]);
+  const [activeProject, setActiveProject] = useState(1);
 
+  function setCurrentProject(currentImages, currentProject) {
+    setlistimages(currentImages);
+    setActiveProject(currentProject);
+  }
 
+  if (images.length > 1) {
+    grey = { background: "#f2f2f2", padding: "2px 2px 2px 2rem" };
+  }
 
-if(images.length>1){
-  grey = {background:"#f2f2f2"}
-
- }
-  
-
-  const projects = ProjectsFile.map((item) => (
-    <div style={grey} key={item.key}>
-      <H5  onClick={() => setlistimages(item.pictures)}>{item.name}</H5>
-      <DETAILS style={{ margin: ".4rem 0 1rem 0" }}>
+  const projects = ProjectsFile.map(item => (
+    <div
+      style={
+        item.key === activeProject ? grey : { padding: "2px 2px 2px 2rem" }
+      }
+      key={item.key}
+    >
+      <H5 onClick={() => setCurrentProject(item.pictures, item.key)}>
+        {item.name}
+      </H5>
+      <DETAILS style={{ margin: "6px 0 0 0" }}>
         View More Details
         <img src='./left-arrow.svg' style={{ marginLeft: ".3rem" }} alt='' />
       </DETAILS>
@@ -74,13 +79,15 @@ if(images.length>1){
 
   return (
     <div
-      style={{ paddingTop: ".5rem", paddingRight: "0" }}
+      style={{ paddingTop: ".5rem", paddingRight: "0", paddingLeft: "0" }}
       id='bg'
       className='bg'
     >
       <div className='half1'>
-        <h1>Project</h1>
-        <H6>View each projects with documentations.</H6>
+        <h1 style={{ paddingLeft: "2rem" }}>Project</h1>
+        <H6 style={{ paddingLeft: "2rem" }}>
+          View each projects with documentations.
+        </H6>
 
         <SCROLL>{projects}</SCROLL>
       </div>
