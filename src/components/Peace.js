834dbb556c642ef6ba8bfb23b59288a0513647c2
Darey <horla@Dareys-MacBook-Pro.local>
2020-04-06 18:29:19 +0100

    ddhddhhdd

--- src/components/Peace.js
+++ src/components/Peace.js
@@ -3,7 +3,16 @@ import React, { Component } from "react";
 export class Peace extends Component {
   render() {
     return (
-      <div className='half2' style={{ position: "relative" }}>
+      <div
+        className='half2'
+        style={{
+          position: "relative",
+          display: "flex",
+          justifyContent: "center",
+          height: "100%",
+          alignItems: "flex-end"
+        }}
+      >
         <img id='peace' src='./peace.svg' alt='' />
       </div>
     );
