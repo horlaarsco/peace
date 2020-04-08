4552f00914c3ea3ba154275f2ba89329037ed16f
Darey <horla@Dareys-MacBook-Pro.local>
2020-04-08 01:54:12 +0100

    hshsh

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,15 +1,13 @@
-import React, { Component, lazy, Suspense } from "react";
+import React, { Component } from "react";
 import Details from "./Details";
-const Skills = lazy(() => import("./Skills"));
+import Skills from "./Skills";
 
 export class Background extends Component {
   render() {
     return (
       <div id='bg' className='bg'>
         <Details />
-        <Suspense fallback={<h1>Loading....</h1>}>
-          <Skills />
-        </Suspense>
+        <Skills />
       </div>
     );
   }
